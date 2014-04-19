a1f6ecfae427b8587f72c9b67ccbdffd19f15081
kashifch <kchaudhry@edx.org>
2014-04-19 19:34:56 +0500

    testing

---
+++ Test/test_Git.py
@@ -0,0 +1,21 @@
+__author__ = 'arbisoft'
+
+import unittest
+from bok_choy.web_app_test import WebAppTest
+from Pages import GitHubSearchPage
+
+
+class TestGitHub(WebAppTest):
+    """
+    Tests for the GitHub site.
+    """
+
+    def test_page_existence(self):
+        """
+        Make sure that the page is accessible.
+        """
+        GitHubSearchPage(self.browser).visit()
+
+
+if __name__ == '__main__':
+    unittest.main()
